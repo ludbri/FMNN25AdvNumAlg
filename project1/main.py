a9bc32a2236cc111f518ae6437e54029595dcddb
Ludwig Brieditis <ludwig.brieditis@gmail.com>
2026-09-07 15:51:10 +0200

    draft of objects handling comms

--- project1/main.py
+++ project1/main.py
@@ -1,24 +1,121 @@
 import numpy as np
 import room
+from mpi4py import MPI
 
 # TODO: move to MPI
 # TODO: move to sparse matrices?
 
 # parameters
-roomsizes = np.array([[1,1],[2,1],[1,1]])
+# roomsizes = np.array([[1,1],[2,1],[1,1]])
 gridsize = 3
 h = 1 / gridsize
+# condition size
+c_size = gridsize - 1
 # fixed wall temperatures:
 T = {"w": 15,
      "h": 40,
      "wf": 5}
+w = 0.8
+
+comm = MPI.Comm.Clone(MPI.COMM_WORLD)
+# processes 1,2,3 have their own room
+# process 0 coordinates? or at least summarizes and plots.
+
+rank = comm.Get_rank()
+
+# using tags to communicate whether or not to stop.
+# Idea suggested by copilot.
+TAG_STOP = 0
+
+if rank == 0:
+    # coordination
+    dirs = np.zeros((2,c_size), dtype='d')
+    neus = np.zeros((2,c_size), dtype='d')
+
+    for k in range(5):
+        # Solve room 2:
+        comm.Send([dirs, MPI.DOUBLE], dest=2, tag=1)
+        comm.Recv([neus, MPI.DOUBLE], source=2, tag=2)
+
+        # Solve room 1 and 3:
+        comm.Isend([neus[0], MPI.DOUBLE], dest=1, tag=1)
+        comm.Isend([neus[1], MPI.DOUBLE], dest=3, tag=2)
+
+        comm.Irecv([dirs[0], MPI.DOUBLE], source=1, tag=3)
+        comm.Recv([dirs[1], MPI.DOUBLE], source=3, tag=4)
+
+        print(f"iter {k}, dirs: {dirs}")
+
+    # Send shutdown commands
+    dummy = np.empty(1, dtype='d')
+    for r in range(1,4):
+        comm.Send([dummy, MPI.DOUBLE], dest=r, tag=TAG_STOP)
 
 
-r1 = room.Room1(0, roomsizes[0], gridsize, T)
-print(r1.solve(10))
+elif rank == 1:
+    # room 1
+    r1 = room.Room1(comm, gridsize, T)
 
-r2 = room.Room2(1, roomsizes[1], gridsize, T)
-print(r2.solve([0,0]))
+    dir = np.zeros(c_size, dtype='d')
+    neu = np.zeros(c_size, dtype='d')
+    status = MPI.Status()
+    while True:
+        comm.Probe(source=0, tag=MPI.ANY_TAG, status=status)
 
-r3 = room.Room3(2, roomsizes[2], gridsize, T)
-print(r3.solve(100))
+        if status.Get_tag() == TAG_STOP:
+            dummy = np.empty(1, dtype='d')
+            comm.Recv(dummy, source=0, tag=TAG_STOP)
+            break
+    
+        comm.Recv([neu, MPI.DOUBLE], source=0, tag=1)
+
+        dir = r1.solve(neu)
+
+        comm.Send([dir, MPI.DOUBLE], dest=0, tag=3)
+
+
+elif rank == 2:
+    # room 2
+    dirs = np.zeros((2,c_size), dtype='d')
+    neus = np.zeros((2,c_size), dtype='d')
+
+    r2 = room.Room2(comm, gridsize, T)
+    status = MPI.Status()
+    while True:
+        comm.Probe(source=0, tag=MPI.ANY_TAG, status=status)
+
+        if status.Get_tag() == TAG_STOP:
+            dummy = np.empty(1, dtype='d')
+            comm.Recv(dummy, source=0, tag=TAG_STOP)
+            break
+
+        comm.Recv([dirs, MPI.DOUBLE], source=0, tag=1)
+
+        neus = r2.solve(dirs)
+
+        comm.Send([neus, MPI.DOUBLE], dest=0, tag=2)
+
+
+elif rank == 3:
+    # room 3
+    dir = np.zeros(c_size, dtype='d')
+    neu = np.zeros(c_size, dtype='d')
+
+    r3 = room.Room3(comm, gridsize, T)
+    status = MPI.Status()
+    while True:
+        comm.Probe(source=0, tag=MPI.ANY_TAG, status=status)
+
+        if status.Get_tag() == TAG_STOP:
+            dummy = np.empty(1, dtype='d')
+            comm.Recv(dummy, source=0, tag=TAG_STOP)
+            break
+    
+        comm.Recv([neu, MPI.DOUBLE], source=0, tag=2)
+
+        dir = r3.solve(neu)
+
+        comm.Send([dir, MPI.DOUBLE], dest=0, tag=4)
+
+
+print(f"Process {rank} finished")
--- project1/room.py
+++ project1/room.py
@@ -1,17 +1,22 @@
 import numpy as np
+from mpi4py import MPI
 from scipy.linalg import solve
+
+
+TAG_STOP = 0
 
 
 class Room:
     """Base class for a room"""
 
-    def __init__(self, idx: int, roomsize: tuple[int,int], gridsize: int):
+    def __init__(self, comm: MPI.Comm, roomsize: tuple[int,int], gridsize: int, msg_shape: tuple[int,int]):
         """Initialize parameters and base matrices"""
-        self.idx = idx
+        self.comm = comm
         self.roomsize = roomsize
         self.h = 1/gridsize
         self.N = tuple(gridsize*r + 1 for r in roomsize)
         self.n_vars = self.N[0] * self.N[1]
+        self.msg_shape = msg_shape  # shape of boundary condition messages
 
         # The matrices are stored as the standard 2-D and 1-D shapes used in solving.
         # i.e. A.shape(n,n), u.shape=b.shape(n,1)
@@ -45,30 +50,48 @@ class Room:
         """Return outgoing boundary condition(s)"""
         raise NotImplementedError("implemented in subclasses")
 
-    def solve(self, cond: np.array) -> np.array:
+    def solve(self, cond: np.array | None) -> np.array:
         """Solve the equation system and return the output condition"""
-        self.set_boundary(cond)
-        self.u = solve(self.A.reshape(self.n_vars, self.n_vars),
-                       self.b.reshape(self.n_vars, 1))
+        if cond is not None:
+            self.set_boundary(cond)
+        self.u = solve(self.A, self.b)
         return self.get_boundary()
+
+    def run(self):
+        """
+        Performs the execution loop.
+
+        The loop is exited when a signal with tag=TAG_STOP is received
+        """
+        status = MPI.Status()
+
+        inb_cond = np.empty(self.msg_shape, dtype='d')
+        outb_cond = np.empty_like(inb_cond)
+
+        while True:
+            self.comm.Probe(source=0, tag=MPI.ANY_TAG, status=status)
+
+            if status.Get_tag() == TAG_STOP:
+                dummy = np.empty(1, dtype='d')
+                self.comm.Recv(dummy, source=0, tag=TAG_STOP)
+                break
+        
+            self.comm.Recv([inb_cond, MPI.DOUBLE], source=0, tag=1)
+
+            outb_cond = self.solve(inb_cond)
+
+            self.comm.Send([outb_cond, MPI.DOUBLE], dest=0, tag=2)
 
 
 
 class Room1(Room):
-    def __init__(self, idx, roomsize, gridsize, T):
-        super().__init__(idx, roomsize, gridsize)
+    def __init__(self, comm: MPI.Comm, gridsize, T):
+        super().__init__(comm, [1,1], gridsize, msg_shape=(gridsize-1,))
 
         self.b = self.b.reshape(*self.N)
 
         # East wall (first in order to later override corners as fixed values)
-        # self.A = self.A.reshape(self.n_vars,self.n_vars)
-        # for x in range(1,self.N[0]-1):
-        #     idx_x = (x+1) * self.N[1] - 1
-        #     self.A[idx_x, :] = 0
-        #     self.A[idx_x, idx_x] = -1
-        #     self.A[idx_x, idx_x-1] = 1  # one step left
-        # # the assumed starting condition for neumann is 0:
-        # # self.b[1:-1,-1] = self.h * 0  # one step right (neumann condition)
+        # the rhs is the Neumann condition and initially 0
         self.A = self.A.reshape(*self.N,*self.N)
         for x in range(1,self.N[0]-1):
             self.A[x,-1, :,:] = 0
@@ -97,14 +120,14 @@ class Room1(Room):
         self.b.reshape(*self.N)[1:-1, -1] = self.h * neumann_cond
 
     def get_boundary(self):
-        dir_1 = self.u.reshape(*self.N)[1:-1,-1]
+        dir_1 = self.u.reshape(*self.N)[1:-1,-1].copy()
         return dir_1
 
 
 
 class Room2(Room):
-    def __init__(self, idx, roomsize, gridsize, T):
-        super().__init__(idx, roomsize, gridsize)
+    def __init__(self, comm: MPI.Comm, gridsize, T):
+        super().__init__(comm, [2,1], gridsize, msg_shape=(2, gridsize-1))
 
         # half vertical wall size:
         self.n_half = self.N[0] // 2 + 1
@@ -122,14 +145,12 @@ class Room2(Room):
         # West wall bottom
         self.A[self.n_half:-1,0, :,:] = 0
         self.A[self.n_half:-1,0, self.n_half:-1, 0] = np.eye(self.c_size)
-        # Note: initial conditions are 0
-        # self.b[self.n_half:-1,0] = 0  # this is where Dirichlet condition from room 1 is used
+        # Note: initial Dirichlet conditions are 0
 
         # East wall top
         self.A[1:self.n_half-1,-1, :,:] = 0
         self.A[1:self.n_half-1,-1, 1:self.n_half-1,-1] = np.eye(self.c_size)
-        # Note: initial conditions are 0
-        # self.b[1:self.n_half-1,-1] = 0  # this is where Dirichlet condition from room 3 is used
+        # Note: initial Dirichlet conditions are 0
         # East wall bottom
         self.A[self.n_half-1:,-1, :,:] = 0
         self.A[self.n_half-1:,-1, self.n_half-1:,-1] = np.eye(self.n_half)
@@ -149,44 +170,43 @@ class Room2(Room):
         self.b = self.b.reshape(self.n_vars,1)
 
         # Matrices for outgoing conditions (Neumann - derivative)
-        self.A_nc1 = np.zeros((self.c_size,*self.N),dtype='d')
-        self.A_nc2 = self.A_nc1.copy()
+        A_nc1 = np.zeros((self.c_size,*self.N),dtype='d')
+        A_nc2 = np.zeros_like(A_nc1)
         for i, x in enumerate(range(1,self.c_size+1)):
             x1 = i + self.n_half  # offset to the lower half of the left wall
             # center
-            self.A_nc1[i, x1,0] = self.A_nc2[i, x,-1] = -3
+            A_nc1[i, x1,0] = A_nc2[i, x,-1] = -3
             # step left
-            self.A_nc2[i, x,-2] = 1
+            A_nc2[i, x,-2] = 1
             # step right
-            self.A_nc1[i, x1,1] = 1
+            A_nc1[i, x1,1] = 1
             # step up
-            self.A_nc1[i, x1-1,0] = 1
-            self.A_nc2[i, x-1,-1] = 1
+            A_nc1[i, x1-1,0] = 1
+            A_nc2[i, x-1,-1] = 1
             # step down
-            self.A_nc1[i, x1+1,0] = 1
-            self.A_nc2[i, x+1,-1] = 1
-        self.A_nc1 = self.A_nc1.reshape(-1,self.n_vars)
-        self.A_nc2 = self.A_nc2.reshape(-1,self.n_vars)
+            A_nc1[i, x1+1,0] = 1
+            A_nc2[i, x+1,-1] = 1
+        A_nc1 = A_nc1.reshape(-1,self.n_vars)
+        A_nc2 = A_nc2.reshape(-1,self.n_vars)
+        self.A_nc = np.array([A_nc1, A_nc2])
 
     def set_boundary(self, dirichlet_conds):
         self.b.reshape(*self.N)[self.n_half:-1,0] = dirichlet_conds[0]
         self.b.reshape(*self.N)[1:self.n_half-1,-1] = dirichlet_conds[1]
 
     def get_boundary(self):
-        neu_1 = self.A_nc1 @ self.u
-        neu_2 = self.A_nc2 @ self.u
-        return neu_1, neu_2
+        neus = self.A_nc @ self.u
+        return neus
 
 
 class Room3(Room):
-    def __init__(self, idx, roomsize, gridsize, T):
-        super().__init__(idx, roomsize, gridsize)
+    def __init__(self, comm: MPI.Comm, gridsize, T):
+        super().__init__(comm, [1,1], gridsize, msg_shape=(gridsize-1,))
 
         self.b = self.b.reshape(*self.N)
 
         # West wall (first in order to later override corners as fixed values)
-        # initial neumann condition is zero
-        # # self.b[1:-1,0] = self.h * 0  # one step left (neumann condition)
+        # the rhs is the Neumann condition and initially 0
         self.A = self.A.reshape(*self.N,*self.N)
         for x in range(1,self.N[0]-1):
             self.A[x,0, :,:] = 0
@@ -215,6 +235,6 @@ class Room3(Room):
         self.b.reshape(*self.N)[1:-1, 0] = self.h * neumann_cond
 
     def get_boundary(self):
-        dir_2 = self.u.reshape(*self.N)[1:-1,0]
+        dir_2 = self.u.reshape(*self.N)[1:-1,0].copy()
         return dir_2
 
