350b7e92e7626a2ea23d5e4809e7bf290bab1a9a
Ludwig Brieditis <ludwig.brieditis@iml.lth.se>
2026-09-07 10:47:39 +0200

    more comments and components implemented without MPI

--- project1/main.py
+++ project1/main.py
@@ -11,13 +11,14 @@ T_w = 15
 T_h = 40
 T_wf = 5
 
+
 # starting with room 1
-room_idx = 2
+room_idx = 1
 N = tuple(gridsize*r + 1 for r in roomsizes[room_idx])
 n_vars = N[0] * N[1]
 u = np.empty(n_vars, dtype='d')
 A = np.zeros((n_vars, n_vars), dtype='d')
-rhs = np.zeros_like(u)
+b = np.zeros_like(u)
 
 
 # constraints for the interior points (rhs=0):
@@ -41,7 +42,7 @@ A += np.diagflat(np.ones(n_vars-abs(k)),k)
 # When A is reshaped as (*N,*N), the indexing is [rhs_i, rhs_j, u_i, u_j]
 # i.e., [0,0,0,:] 
 A = A.reshape(*N,*N)
-rhs = rhs.reshape(*N)
+b = b.reshape(*N)
 
 
 
@@ -53,28 +54,28 @@ if room_idx == 0:
     # East wall (first in order to later override corners as fixed values)
     A = A.reshape(n_vars,n_vars)
     for i, x in enumerate(range(1,N[0]-1)):
-        idx_x = x * N[1] + (N[1] - 1)
+        idx_x = (x+1) * N[1] - 1
         A[idx_x, :] = 0
-        A[idx_x, idx_x] = 1
-        A[idx_x, idx_x+1] = -1  # one step right
+        A[idx_x, idx_x] = -1
+        A[idx_x, idx_x-1] = 1  # one step left
         # TODO: should this flip sign (to +)? the message is outgoing derivative, receiver uses incoming?
-        rhs[x,-1] = h * neu_1[i]  # one step right (neumann condition)
+        b[x,-1] = h * neu_1[i]  # one step right (neumann condition)
     A = A.reshape(*N,*N)
 
     # North wall
     A[0, :, :, :] = 0
     A[0, :, 0, :] = np.eye(N[1])
-    rhs[0, :] = T_w
+    b[0, :] = T_w
 
     # South wall
     A[-1, :, :, :] = 0
     A[-1, :, -1, :] = np.eye(N[1])
-    rhs[-1, :] = T_w
+    b[-1, :] = T_w
 
     # West wall
     A[:,0,:,:] = 0
     A[:,0,:,0] = np.eye(N[0])
-    rhs[:,0] = T_h
+    b[:,0] = T_h
 
 
 
@@ -82,39 +83,41 @@ if room_idx == 0:
 elif room_idx == 1:
     # half vertical wall size:
     n_half = N[0] // 2 + 1
+    # condition size
+    c_size = n_half - 2
     # incoming Dirichlet boundary conditions
-    dir_1 = np.array([0]*(n_half-1))
-    dir_2 = np.array([0]*(n_half-1))
+    dir_1 = np.array([0]*(c_size))
+    dir_2 = np.array([0]*(c_size))
 
     # Note: east/west before north/south to override corners with fixed heat walls
 
     # West wall top
     A[:n_half,0, :,:] = 0
     A[:n_half,0, :n_half,0] = np.eye(n_half)
-    rhs[:n_half,0] = T_w
+    b[:n_half,0] = T_w
     # West wall bottom
-    A[n_half:,0, :,:] = 0
-    A[n_half:,0, n_half:,0] = np.eye(n_half-1)
-    rhs[n_half:,0] = dir_1  # this is where Dirichlet condition from room 1 is used
+    A[n_half:-1,0, :,:] = 0
+    A[n_half:-1,0, n_half:-1, 0] = np.eye(c_size)
+    b[n_half:-1,0] = dir_1  # this is where Dirichlet condition from room 1 is used
 
     # East wall top
-    A[:n_half-1,-1,:,:] = 0
-    A[:n_half-1,-1,:n_half-1,-1] = np.eye(n_half-1)
-    rhs[:n_half-1,-1] = dir_2  # this is where Dirichlet condition from room 3 is used
+    A[1:n_half-1,-1, :,:] = 0
+    A[1:n_half-1,-1, 1:n_half-1,-1] = np.eye(c_size)
+    b[1:n_half-1,-1] = dir_2  # this is where Dirichlet condition from room 3 is used
     # East wall bottom
-    A[n_half-1:,-1,:,:] = 0
-    A[n_half-1:,-1,n_half-1:,-1] = np.eye(n_half)
-    rhs[n_half-1:,-1] = T_w
+    A[n_half-1:,-1, :,:] = 0
+    A[n_half-1:,-1, n_half-1:,-1] = np.eye(n_half)
+    b[n_half-1:,-1] = T_w
 
     # North wall
     A[0, :, :, :] = 0
     A[0, :, 0, :] = np.eye(N[1])
-    rhs[0, :] = T_h
+    b[0, :] = T_h
 
     # South wall
     A[-1, :, :, :] = 0
     A[-1, :, -1, :] = np.eye(N[1])
-    rhs[-1, :] = T_wf
+    b[-1, :] = T_wf
 
 
 
@@ -127,10 +130,10 @@ if room_idx == 2:
     for i, x in enumerate(range(1,N[0]-1)):
         idx_x = x * N[1]
         A[idx_x, :] = 0
-        A[idx_x, idx_x] = 1
-        A[idx_x, idx_x+1] = -1  # one step left
+        A[idx_x, idx_x] = -1
+        A[idx_x, idx_x+1] = 1  # one step right
         # TODO: should this flip sign (to +)? the message is outgoing derivative, receiver uses incoming?
-        rhs[x,0] = h * neu_2[i]  # Neumann condition
+        b[x,0] = h * neu_2[i]  # Neumann condition
     A = A.reshape(*N,*N)
 
     # West wall (first in order to later override corners as fixed values)
@@ -141,42 +144,67 @@ if room_idx == 2:
     # North wall
     A[0, :, :, :] = 0
     A[0, :, 0, :] = np.eye(N[1])
-    rhs[0, :] = T_w
+    b[0, :] = T_w
 
     # South wall
     A[-1, :, :, :] = 0
     A[-1, :, -1, :] = np.eye(N[1])
-    rhs[-1, :] = T_w
+    b[-1, :] = T_w
 
     # East wall
     A[:,-1,:,:] = 0
     A[:,-1,:,-1] = np.eye(N[0])
-    rhs[:,-1] = T_h
+    b[:,-1] = T_h
 
 
 
 
 # reshape back before solving
 A = A.reshape(n_vars,n_vars)
-rhs = rhs.reshape(n_vars)
+b = b.reshape(n_vars)
 # Note: corner points do not matter, as they are only attached to neighbours with fixed heat.
 
 # printouts for dev
-print(np.arange(n_vars,dtype='d').reshape(N[0],-1))
-print(np.arange(n_vars,dtype='d'))
-print(A.reshape(n_vars,n_vars))
-print(np.arange(n_vars,dtype='d'))
-print(rhs.reshape(1,-1))
+idxs =np.arange(n_vars,dtype='d') 
+print(idxs.reshape(N[0],-1))
+# print(idxs)
+# print(A.reshape(n_vars,n_vars))
+# print(idxs)
+# print(b.reshape(1,-1))
 
-sol = solve(A, rhs)
-print(sol.reshape(*N))
+u = solve(A, b)
+print(u.reshape(*N))
 
 
 # depending on room, compute the output boundary condition
 if room_idx == 0:
-    ...
+    # Outgoing condition is Dirichlet (heat)
+    dir_1 = u.reshape(*N)[1:-1,-1]
 elif room_idx == 1:
-    ...
+    # Outgoing condition is Neumann (gradient)
+    A_nc1 = np.zeros((c_size,n_vars),dtype='d')
+    A_nc2 = A_nc1.copy()
+    for i, x in enumerate(range(1,c_size+1)):
+        idx_x1 = (i + n_half) * N[1]  # left wall
+        idx_x2 = x * N[1] + (N[1] - 1)  # right wall
+        # center
+        A_nc1[i,idx_x1] = A_nc2[i,idx_x2] = -3
+        # step left
+        A_nc2[i, idx_x2-1] = 1
+        # step right
+        A_nc1[i, idx_x1+1] = 1
+        # step up
+        A_nc1[i, idx_x1-N[1]] = 1
+        A_nc2[i, idx_x2-N[1]] = 1
+        # step down
+        A_nc1[i, idx_x1+N[1]] = 1
+        A_nc2[i, idx_x2+N[1]] = 1
+    print(A_nc1)
+    print("NC1", A_nc1@u)
+    print(A_nc2)
+    print("NC2", A_nc2@u)
+
 elif room_idx == 2:
-    ...
+    # Outgoing condition is Dirichlet (heat)
+    dir_2 = u.reshape(*N)[1:-1,0]
 
--- project1/readme.md
+++ project1/readme.md
@@ -26,12 +26,11 @@ There are heaters on:
 The south wall of 2 has a big window
 
 The wall temperatures are:
-- normal wall, u(x)=15    (assumed only outer walls and not G1, G2?)
+- normal wall, u(x)=15    (only outer walls)
 - heated wall, u(x)=40
 - wall with windows, u(x)=5
 
-
-
+# Modelling
 The temperature at the point $x$ is $u(x)$.
 $x$ is modelled using matrix coordinates, starting in the top-left with x_1 vertical, x_2 horizontal.
 
@@ -41,11 +40,80 @@ $$\Delta u(x) = 0, x\in\Omega\subset \mathbb{R}^2$$
 To solve it, it is first discretized on a grid with spacing $\Delta x$.
 
 The discrete approximation on the grid is denoted $u_{i,j}\approx u(x_{i,j})$. Then, the 2nd order differences are:
-$$\Delta u(x_{i,j}) \approx \frac{u_{i,j+1}+u_{i,j-1}-4u_{i,j}+u_{i+1,j}+u_{i-1,j}}{\Delta x^2}.$$
+$$\Delta u(x_{i,j}) \approx \frac{u_{i,j+1}+u_{i,j-1}-4u_{i,j}+u_{i+1,j}+u_{i-1,j}}{\Delta x^2}$$,
+where $\Delta x = h > 0$ is the distance between discretization points along each dimension.
 
 
+# Solution approach:
+For each room and iteration, the heat values are found by solving $Au=b$. $A$ is a $|u|$x$|u|$ matrix and $u$ and $b$ are column vectors.
 
-Idea: start by using Dirichlet (heat) conditions to solve room 2.
+Start by using Dirichlet (heat) conditions to solve room 2.
 The solution gives Neumann (heat derivative) conditions for use in rooms 1&3.
 Solve 1&3.
-Update uk with exponential smoothing, and keep iterating.
+Update $u^k$ with exponential smoothing, and keep iterating.
+
+## Condition types
+For each discretized point $u_{i,j}$, there is a corresponding condition in $A$ and $b$:
+- If $x_{i,j}\in\Gamma_h$, then $u_{i,j}=T_h$
+- If $x_{i,j}\in\Gamma_{wh}$, then $u_{i,j}=T_{wh}$
+- If $x_{i,j}\in\Gamma_w$, then $u_{i,j}=T_w$
+- If $x_{i,j}\in\Gamma_1\setminus(\Gamma_1\cap\Gamma_{\delta\Omega})$ and room 1, then [incoming Neumann 1]
+- If $x_{i,j}\in\Gamma_1\setminus(\Gamma_1\cap\Gamma_{\delta\Omega})$ and room 2, then $u_{i,j}=u_{DC1}(j)$
+- If $x_{i,j}\in\Gamma_2\setminus(\Gamma_2\cap\Gamma_{\delta\Omega})$ and room 3, then [incoming Neumann 2]
+- If $x_{i,j}\in\Gamma_2\setminus(\Gamma_2\cap\Gamma_{\delta\Omega})$ and room 2, then $u_{i,j}=u_{DC2}(j)$
+- If $x_{i,j}$ is an interior point, then $\Delta u_{i,j}=0$
+
+## Assumptions and observations:
+- For $k=0$, it is assumed that the [Dir] and [Neu] conditions are equal to 0.
+- It is assumed that boundary points (corners) that are in both the outer boundary $\delta\Omega$ and $\Gamma_1$ or $\Gamma_2$ are only in $\delta\Omega$.
+- The western (eastern) corners in room 1 (3) have no influence on the solution as their neighbours are always other walls with fixed temperature. Thus, their condition is irrelevant.
+The same is true for the NW and SE corners of room 2.
+- The NE (SW) corder of room 1 (3) is always a standard wall no matter which room is evaluated. 
+- The SE (NW) corner of room 1 (3) is treated according to the room currently considerd: when room 1 (3) is evaluated, it is a standard wall with $T_w$, when room 2 is evaluated, it is a $T_{wh}$ ($T_h$) wall. This has no impact on the result, as the neighours are both with fixed temperature when room 2 is evaluated and it will ultimately only matter when it is a standard wall.
+
+## Computing the outgoing boundary conditions from each domain:
+- The outgoing Dirichlet condition (DC) from rooms 1 and 3 is simply the resulting value of $u_{i,j}$ in the boundary.
+- The outgoing Neumann conditions (NC) from room 2 are computed
+
+From the discretized Lagrange equation, the outgoing Dirichlet condition (here in direction (0,1)) is:
+$$\frac{u_{i,j+1}+u_{i,j-1}-4u_{i,j}+u_{i+1,j}+u_{i-1,j}}{h^2} = 0$$
+$$\iff \frac{u_{i,j+1}+u_{i,j-1}-4u_{i,j}+u_{i+1,j}+u_{i-1,j}}{\Delta x^2} = -\frac{u_{DC}(i)}{h^2}$$
+$$\iff u_{i,j+h}-4u_{i,j}+u_{i-h,j}+u_{i,j-h} = -u_{DC}(i)$$
+
+From the discretized Lagrange equation, the outgoing Neumann condition (here in direction (0,1)), $u_{NC}(i)=\frac{u_{i,j+1}-u_{i,j}}{h}$ is:
+$$\frac{u_{i,j+1}+u_{i,j-1}-4u_{i,j}+u_{i+1,j}+u_{i-1,j}}{h^2} = 0$$
+$$\iff \frac{u_{NC}(i)}{h}+\frac{u_{i,j-1}-3u_{i,j}+u_{i+1,j}+u_{i-1,j}}{h^2} = 0$$
+$$\iff \frac{u_{i,j-1}-3u_{i,j}+u_{i+1,j}+u_{i-1,j}}{h^2} = - \frac{u_{NC}(i)}{h}$$
+$$\iff \frac{u_{i,j-1}-3u_{i,j}+u_{i+1,j}+u_{i-1,j}}{h} = - u_{NC}(i)$$
+
+
+## Incoming boundary conditions
+For the incoming Dirichlet conditions, the receiving sub-domain includes the constraint $u_{i,j}=u_DC(i)$ for $x_{i,j}\in\Gamma_n$.
+
+For the incoming Neumann conditions, the receiving sub-domain includes the constraint (here in direction $(0,1)$) $u_{i,j+1}-u_{i,j}=u_{NC}(i)$.
+
+
+
+
+## Boundary condition exploration example
+So the question on NC is which side to include, consider the reduced 1-d example of points
+
+$\Omega = \{0, 1, 2, 3, 4\}$
+Let $\Omega_1 = \{0,1,2\}$, $\Omega_2 = \{2,3,4\}$, $\Gamma_1=2$
+
+Then, the Dirichlet condition is simply $u_3$.
+On evaluation of $\Omega_1$, $u_2$ is free to vary.
+On evaluation of $\Omega_2$, $u_3$ is fixed to the DC value.
+On $\Omega 1$, it will be solved from the condition related to point $x_{i-1,j}$. - $u_{DC}(i)$ is simply recovered from the solution $u$ as if it was an orderinary point.
+
+The Neumann condition is from $\Omega_2$ to $\Omega_1$. The question is, is it the derivative from $3\rightarrow 2$ or from $2\rightarrow 1$?
+According to lecture notes, it is the derivative from the boundary point outwards, i.e., $2\rightarrow 1$!
+So, on $\Omega_2$, it is recovered from the solution $u_{\Omega_2}^k$ as $\\$
+(1d case) $\frac{u_{3}-u_{2}}{h} = - u_{NC}$ $\\$
+(2d case, direction $(0,1)$) $\frac{u_{i,j-1}-3u_{i,j}+u_{i+1,j}+u_{i-1,j}}{h} = - u_{NC}(i)$.
+So, it can be evaluated efficiently as a matrix operation with a precomputed matrix.
+
+
+
+# Error bound of discretization
+...
